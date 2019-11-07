c46c54c2d5da517db2a2e21eb166c295450f67ca
Matthew_Heyert <heyerthb@gmail.com>
2019-11-07 08:39:07 -0800

    whiteboard and read me

---
+++ 401/trees/breadth-first/breadth-first.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// class Tree {
+//   constructor(){
+//     this.root = null;
+//   }
+// };
+
+// class Node {
+//   constructor(){
+//     this.left = null,
+//     this.right = null,
+//     this.value = value,
+//   }
+// }
+
+// const Q = new Queue {
+//   constructor(){
+// this.current = null,
+//   }
+// }
+
+// function breadthFirst(){
+//   let values =[];
+//   if (root = null)
+//   return;
+// if ()
+//   let values =[];
+
+
+//   return values;
+// }
+
+
