c55ac11a189340e5f3449e78736237c87c05f76b
Matthew_Heyert <heyerthb@gmail.com>
2019-11-08 07:33:56 -0800

    read me and whiteboard

---
+++ 401/trees/max-value/max-value.js
@@ -0,0 +1,26 @@
+'use strict';
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree{
+  constructor(){
+    this.root = null;
+  }
+}
+
+function maxValue(){
+  if (tree.root === null){
+    return 0;
+  } if 
+}
+
+const tree = new BinaryTree();
+tree.root = new Node(2);
+tree.root.left = new Node(7);
+tree.root.right = new Node(5);
